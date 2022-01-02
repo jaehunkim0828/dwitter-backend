d1aeabbc757a46bdbc71d90f0a9b9fe9602647ff
JaeHun Kim <kkaa81@naver.com>
2022-01-02 15:19:55 +0900

    feat(mvc): create direactory about mvc pattern

---
+++ controller/tweet.js
@@ -0,0 +1,36 @@
+import * as tweetRepository from '../data/tweet.js';
+
+export async function getTweets(_, res) {
+    const tweets = await tweetRepository.getAll();
+    res.send(tweets)
+};
+
+export async function getTweet(req, res) {
+    const { user } = req.params;
+    const userTwits = await tweetRepository.getUser(user);
+    res.status(200).send(userTwits);  //특정 트윗 보여주기.
+}
+
+export async function createTweet(req, res,) {
+    const { name, time, text } = req.body;
+    await tweetRepository.create(name, time, text);
+    res.status(201).send('CREATED twit'); //twit 생성
+}
+
+export async function updateTweet(req, res) {
+    const { name, time, text } = req.body;
+    const isSucceed = await tweetRepository.update(name, time, text);
+    console.log(isSucceed);
+    if (isSucceed) return res.status(200).send('secceed PUT method');
+    return res.status(404).send('fail PUT method');
+}
+
+export async function deleteTweet(req, res) {
+    const { name, time, text } = req.body;
+    const isSucceed = await tweetRepository.remove(name, time, text);
+    console.log(isSucceed);
+    return isSucceed ? 
+        res.status(204).send('secceed DELETE method') 
+            :
+        res.status(404).send('fail DELETE method')
+}
---
+++ data/tweet.js
@@ -0,0 +1,44 @@
+const tweets = [
+    { 
+        name: 'Jaehun', 
+        time: new Date(), 
+        text: 'hello my name is jaehun i want to refund',
+    }
+];
+
+export async function getAll() {
+    return tweets;
+}
+
+export async function create(name, time, text) {
+    if (!name || !time || !text) return res.status(404).send('fail twit');
+    tweets.push({ name, time, text }); 
+    return;
+}
+
+export async function update(name, time, text) {
+    let bool = false;
+    tweets.map((tweet, i) => {
+        if (tweet.name === name && tweet.time === time) {
+            tweets[i] = { name, time, text };
+            bool = true;
+        }
+    })
+    return bool ? true : false;
+}
+
+export async function remove(name, time, text) {
+    let bool = false;
+    tweets.map((tweet, i ) => {
+        if (tweet.name === name && tweet.time === time && tweet.text === text) {
+            tweets.splice(i, 1);
+            bool = true;
+        }
+    })
+    
+    return bool ? true : false;
+}
+
+export async function getUser(user) {
+    return tweets.filter(twit => twit.name === user);
+}
