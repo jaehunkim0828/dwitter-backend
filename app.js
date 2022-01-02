1619fd717b5d065cab62aa1dc546926f1a50ce36
JaeHun Kim <kkaa81@naver.com>
2022-01-02 15:18:24 +0900

    fix(middleware): change middleware

--- app.js
+++ app.js
@@ -14,19 +14,32 @@
 //6. '/twit/:user' put
 //7. '/twit/:user' delete
 import express from 'express';
+import cors from 'cors';
+// import morgan from 'morgan';
+// import helmet from 'helmet';
+import 'express-async-errors';
 
 import rootRouter from './routes/index.js';
 
 const app = express();
-const port = '3000';
+const port = 3000;
 
 app.use(express.json());
+// app.use(helmet());
+app.use(cors());
+// app.use(morgan('tiny'));
+
 
 app.use('/', rootRouter);
 
-// app.use((error, req, res, next) => {
-//     console.error(error);
-//     res.status(500).json({ message: 'Something went wrong' });
-//   });
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+})
+
 
 app.listen(port, () => console.log(`server is running http://localhost:${port}`));
