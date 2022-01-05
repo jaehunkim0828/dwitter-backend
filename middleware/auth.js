bf88cab24bde8f37a07635342c7d58bd428a4e52
JaeHun Kim <kkaa81@naver.com>
2022-01-05 18:05:49 +0900

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 import * as authRepository from '../data/auth.js';
+import { config } from '../config.js';
 
 const AUTH_ERROR = { message: 'Authentication Error '};
 const secretKey = 'random';
@@ -7,18 +8,24 @@ const secretKey = 'random';
 export const isAuth = async (req, res, next) => {
     const authHeader = req.get('Authorization');
     if (!(authHeader && authHeader.startsWith('Bearer '))) {
-        console.log('he');
         return res.status(401).json(AUTH_ERROR);
     }
+    console.log(config.secretKey);
 
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, secretKey, async (error, decoded) => {
+    
+    jwt.verify(
+        token, 
+        secretKey, 
+        async (error, decoded) => {
+        
         if (error) {
-            console.log(1);
+            console.log('token is null');
             return res.status(401).json(AUTH_ERROR);
         }
         const user = await authRepository.getById(decoded.id);
         if (!user) {
+            console.log()
             console.log(2);
             return res.status(401).json(AUTH_ERROR);
         }
